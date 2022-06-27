56834f6345a402a6ba788ddec2d03860a054b9cb
bagussatriaa <satria.bsw03@gmail.com>
2022-06-27 16:43:17 +0700

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const { query } = require('express');
 const express = require('express');
 
 const app = express();
-const port = 300;
 const db = require('./connection/database');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const flash = require('express-flash');
+const upload = require('./middlewares/fileUpload');
+
+const port = 300;
+
 app.set('view engine', 'hbs');
-
+app.use(
+  session({
+    secret: 'session',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+  })
+);
 const dataBlog = [];
-app.use('/assets', express.static(__dirname + '/assets'));
-app.use(express.urlencoded({ extended: false }));
 
-const isLogin = true;
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
 
 db.connect((err, client, done) => {
   if (err) throw err;
@@ -28,45 +43,27 @@ db.connect((err, client, done) => {
         return {
           ...item,
           duration: takeTheTime(item.start_date, item.end_date),
-
-          isLogin,
+          isLogin: req.session.isLogin,
         };
       });
       // console.log(dataBlog);
-      res.render('index', { isLogin, blogs: dataBlog });
+      res.render('index', { blogs: dataBlog, user: req.session.user, isLogin: req.session.isLogin });
     });
   });
 
-  app.get('/register', (req, res) => {
-    res.render('register');
-  });
-  app.get('/login', (req, res) => {
-    res.render('login');
-  });
-
   app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', { user: req.session.user, isLogin: req.session.isLogin });
   });
 
   app.get('/project', (req, res) => {
-    res.render('project');
+    res.render('project', { user: req.session.user, isLogin: req.session.isLogin });
   });
 
-  app.post('/project', (req, res) => {
-    // data = {
-    //   projectName: data.project_name,
-    //   startDate: data.start_date,
-    //   endDate: data.end_date,
-    //   duration: takeTheTime(data.start_date, data.end_date),
-    //   desc: data.desc,
-    //   image: data.image,
-    //   icons,
-    //   isLogin,
-    // };
-    // console.log(data);
+  app.post('/project', upload.single('image'), (req, res) => {
     let data = req.body;
 
-    // "${data.icons.java}","${data.icons.bootstrap}"
+    const image = req.file.filename;
+    // console.log(image);
     let icons = {
       vuejs: req.body.vuejs,
       js: req.body.js,
@@ -74,9 +71,9 @@ db.connect((err, client, done) => {
       bootstrap: req.body.bootstrap,
     };
     let query = `INSERT INTO tb_projects(
-      "projectName", "desc" , start_date, end_date, icons)
+      "projectName", "desc" , start_date, end_date, icons, image)
         VALUES ('${data.project_name}', '${data.desc}', '${data.start_date}', '${data.end_date}', 
-        '{"${icons.vuejs}","${icons.js}","${icons.java}","${icons.bootstrap}"}')`;
+        '{"${icons.vuejs}","${icons.js}","${icons.java}","${icons.bootstrap}"}','${image}')`;
     client.query(query, (err, result) => {
       if (err) throw err;
 
@@ -84,29 +81,15 @@ db.connect((err, client, done) => {
     });
   });
 
-  app.get('/blog-detail/:id', (req, res) => {
+  app.get('/blog-detail/:id', upload.single('image'), (req, res) => {
     let id = req.params.id;
     console.log(id);
-    let query = 'SELECT * FROM tb_projects';
+    let query = `SELECT * FROM tb_projects WHERE id=${id}`;
     client.query(query, (err, result) => {
       if (err) throw err;
-      // let icons = {
-      //   vuejs: .vuejs,
-      //   js: req.body.js,
-      //   java: req.body.java,
-      //   bootstrap: req.body.bootstrap,
-      // };
-      // detaildata = dataBlog[id];
-      // console.log(result.rows[0]);
+
       let detaildata = result.rows[0];
-      console.log(detaildata);
-      // let icons = {
-      //   vuejs: detaildata.icons[0],
-      //   js: detaildata.icons[1],
-      //   java: detaildata.icons[2],
-      //   bootstrap: detaildata.icons[3],
-      // };
-      // console.log(icons);
+      // console.log(detaildata);
       detaildata = {
         projectName: detaildata.projectName,
         start_date: detaildata.start_date,
@@ -119,9 +102,10 @@ db.connect((err, client, done) => {
           java: detaildata.icons[2],
           bootstrap: detaildata.icons[3],
         },
+        image: detaildata.image,
       };
-      // console.log(detaildata);
-      res.render('blog-detail', { id, isLogin, detaildata });
+      console.log(detaildata.image);
+      res.render('blog-detail', { id, detaildata, user: req.session.user, isLogin: req.session.isLogin });
     });
   });
 
@@ -145,7 +129,7 @@ db.connect((err, client, done) => {
       console.log(dataBlog);
       // let update = dataBlog[id];
       // console.log(update);
-      res.render('edit-blog', { id, update: dataBlog });
+      res.render('edit-blog', { id, update: dataBlog, user: req.session.user, isLogin: req.session.isLogin });
     });
     // console.log(update);
   });
@@ -162,12 +146,72 @@ db.connect((err, client, done) => {
     };
     console.log(dataUpdate);
     let query = `UPDATE tb_projects
-      SET "projectName"='${dataUpdate.project_name}', "desc"='${dataUpdate.desc}',  start_date='${dataUpdate.start_date}', end_date='${dataUpdate.end_date}', icons= '{"${iconsUpdate.vuejs}","${iconsUpdate.js}","${iconsUpdate.java}","${iconsUpdate.bootstrap}"}'
-        
+      SET "projectName"='${dataUpdate.project_name}', "desc"='${dataUpdate.desc}',  start_date='${dataUpdate.start_date}', end_date='${dataUpdate.end_date}', icons= '{"${iconsUpdate.vuejs}","${iconsUpdate.js}","${iconsUpdate.java}","${iconsUpdate.bootstrap}"}, '
       WHERE id = ${id};`;
+    // image='${dataUpdate.image}',
     client.query(query, (err, result) => {
       if (err) throw err;
     });
+    res.redirect('/');
+  });
+
+  app.get('/register', (req, res) => {
+    res.render('register', { user: req.session.user, isLogin: req.session.isLogin });
+  });
+
+  app.post('/register', (req, res) => {
+    let { regName, regEmail, regPw } = req.body;
+    console.log(regPw);
+    const hashPw = bcrypt.hashSync(regPw, 10);
+
+    const query = `INSERT INTO tb_user(name, email, password)
+    VALUES ('${regName}', '${regEmail}', ' ${hashPw}');`;
+    client.query(query, (err, result) => {
+      if (err) throw err;
+
+      res.redirect('/login');
+    });
+  });
+
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  app.post('/login', (req, res) => {
+    let { regEmail, regPw } = req.body;
+    console.log(regPw);
+
+    client.query(`SELECT * FROM tb_user WHERE email='${regEmail}'`, (err, result) => {
+      if (err) throw err;
+
+      console.log(result.rows[0]);
+      if (result.rows.length == 0) {
+        req.flash('notRegis', 'Unregister');
+        return res.redirect('/register');
+      }
+      const isMatch = bcrypt.compareSync(regPw, result.rows[0].password);
+      console.log(isMatch);
+      console.log(result.rows[0].password);
+
+      if (isMatch !== true) {
+        req.session.isLogin = true;
+        req.session.user = {
+          id: result.rows[0].id,
+          name: result.rows[0].name,
+          email: result.rows[0].email,
+        };
+        req.flash('success', 'Login success');
+        res.redirect('/');
+      } else {
+        req.flash('danger', 'wrong password');
+        res.redirect('/login');
+      }
+    });
+  });
+
+  app.get('/logout', (req, res) => {
+    req.session.destroy();
+
     res.redirect('/');
   });
 });
